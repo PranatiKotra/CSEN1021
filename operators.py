18a7237b48ce3c33fff9d6ed6ae394dcdcf5e207
PranatiKotra <94438529+PranatiKotra@users.noreply.github.com>
2022-03-23 11:03:31 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,16 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+sum=a+b
+sub=a-b
+mul=a*b
+div=a/b
+rem=a%b
+pow=a**b
+result=a//b
+print(f"Sum of {a} and {b} is {sum}")
+print(f"Subtraction of {a} and {b} is {sub}")
+print(f"Multiplication of {a} and {b} is {mul}")
+print(f"Division of {a} and {b} is {div}")
+print(f"Reminder of {a} and {b} is {rem}")
+print(f"Power of {a} over {b} is {pow}")
+print(f"{a} // {b} is {result}")
