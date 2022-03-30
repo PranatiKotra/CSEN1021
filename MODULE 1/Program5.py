4d0a51f939f9e13d24dc974b746a835d84a74076
PranatiKotra <94438529+PranatiKotra@users.noreply.github.com>
2022-03-30 11:42:14 +0530

    Create Program5.py

---
+++ MODULE 1/Program5.py
@@ -0,0 +1,9 @@
+a=float(input("Enter a number:"))
+import math
+print(math.ceil(a))
+print(math.floor(a))
+print(math.fabs(a))  
+print(math.fmod(a))
+print(math.trunc(a))
+print(math.pow(a,2))
+print(math.sqrt(a))
